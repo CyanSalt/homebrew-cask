aa2896249313ee35e7d74dc583e1e4bb939f4501
CyanSalt <c.salt@hotmail.com>
2022-02-10 18:33:37 +0800

    feat: release commas@0.20.0

--- Casks/commas.rb
+++ Casks/commas.rb
@@ -1,6 +1,6 @@
 cask "commas" do
-  version "0.19.0"
-  sha256 "5a8466190f342ae9610afe273c55496dd1743d20cf80364f4d7fd4d898d9d3ca"
+  version "0.20.0"
+  sha256 "be18ecca2ded26d6589b7bba2ab1f52ab36d27a993b9e313951685d004ae9ed2"
 
   url "https://github.com/CyanSalt/commas/releases/download/v#{version}/Commas-darwin-x64.zip"
   name "Commas"
--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Or `brew tap cyansalt/cask` and then `brew install --cask <formula>`.
 ## Documentation
 
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+
+## How to submit a new version?
+
+Just update the `version` and generate `sha256` with the `shasum -a 256 <file>.zip` command.
