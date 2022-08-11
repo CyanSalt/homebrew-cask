6cafa8cadbb4eef1a8729f9ef6dc9657685a2e29
CyanSalt <c.salt@hotmail.com>
2022-08-11 13:56:44 +0800

    feat: release commas@0.22.0

--- Casks/commas.rb
+++ Casks/commas.rb
@@ -1,6 +1,6 @@
 cask "commas" do
-  version "0.21.3"
-  sha256 "232407be8153e2a04463fbf41e4c12c12b06ac59db568f1545bb787d0330caeb"
+  version "0.22.0"
+  sha256 "917dec395e2f23e5d60a38565a4fae8f4986b6bcf9a7a1c0f782a3808283632d"
 
   url "https://github.com/CyanSalt/commas/releases/download/v#{version}/Commas-darwin-x64.zip"
   name "Commas"
