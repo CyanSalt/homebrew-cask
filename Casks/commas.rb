7bf32d1fde070f9a4ccb4c52ffe2b87a1d49fce1
CyanSalt <c.salt@hotmail.com>
2022-08-11 14:17:49 +0800

    fix: bad hash for commas

--- Casks/commas.rb
+++ Casks/commas.rb
@@ -1,6 +1,6 @@
 cask "commas" do
   version "0.22.0"
-  sha256 "917dec395e2f23e5d60a38565a4fae8f4986b6bcf9a7a1c0f782a3808283632d"
+  sha256 "6a49072b75ae939c50e410c0dd9f648f5b13f72b2fb1e79013b0b001624d7eba"
 
   url "https://github.com/CyanSalt/commas/releases/download/v#{version}/Commas-darwin-x64.zip"
   name "Commas"
