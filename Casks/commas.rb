decae3a6d3a36f9813005840099a6310344f7810
CyanSalt <c.salt@hotmail.com>
2023-10-24 20:41:45 +0800

    feat: release commas@0.29.2

--- Casks/commas.rb
+++ Casks/commas.rb
@@ -1,9 +1,9 @@
 cask "commas" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.29.1"
-  sha256 arm:   "5a7185604fdcd1144290e7ca3672bf5935c5f38df8c78e417cb70f5d538188f8",
-         intel: "0aff1e833409750a1be73de1eeadb7cc3b8c0b0d77fd666a4c48f70dc4cc5f26"
+  version "0.29.2"
+  sha256 arm:   "8997f2a586b45fce384213a4c1743eb4b3832af180e95cf1d6e20da9bba451ca",
+         intel: "fe5104247a8c7d13556595130d9fbd356386524ee264d8d7d1109df144b1956c"
 
   url "https://github.com/CyanSalt/commas/releases/download/v#{version}/Commas-darwin-#{arch}.zip"
   name "Commas"
