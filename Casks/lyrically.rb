fef5cbcf65e0cee524ef63faccf2a31117041133
CyanSalt <c.salt@hotmail.com>
2025-04-10 21:07:55 +0800

    feat: release lyrically@0.4.0

--- Casks/lyrically.rb
+++ Casks/lyrically.rb
@@ -1,9 +1,9 @@
 cask "lyrically" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.3.1"
-  sha256 arm:   "a181991ee2173e5bd6da600608277b2c746a89238fa350120a0ba22ca8b03047",
-         intel: "e9bb84c318e71cc4b0a9b47001d7f3942098c62c4d533746d24d3283a9ef6eb5"
+  version "0.4.0"
+  sha256 arm:   "d8de6cfe166bc060e158eba09f45d6c2d5a7a7f29c9e4d6b2dcfd9ade73e0653",
+         intel: "cf04c9b256261b14993d3faa59ec94091149cadeee8009a89cd9c372a6de5a45"
 
   url "https://github.com/CyanSalt/lyrically/releases/download/v#{version}/Lyrically-darwin-#{arch}.zip"
   name "Lyrically"
